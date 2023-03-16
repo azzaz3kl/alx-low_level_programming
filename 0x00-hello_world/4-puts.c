c2e6a355608cb97fe6b727065f383761f2135545
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-03-16 21:44:29 +0200

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 /*
- *main - a function that print a string for user. Is the main function for all program.
- *puts - a predefined function that use to print outputs for the user.
- return (0) - means program executed successfully.
- * */
+ *main - a function that print a string for user using a puts function.
+ *return (0) - Success.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

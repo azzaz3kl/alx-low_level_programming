f913b6465cd656a58a8a17d523f9686800924763
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-03-16 23:39:55 +0200

    Test8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - a program to print a quote into error mode
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}

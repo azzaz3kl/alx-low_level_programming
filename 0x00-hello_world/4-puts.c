cb119e1bb7584e98f13290f4fc69ed6111c093d6
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-03-16 21:50:01 +0200

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/*
- *main - a function that print a string for user using a puts function.
- *return (0) - Success.
+/**
+ * main - a function that print a string for user using a puts function.
+ * Return: 0 (Success).
  */
 int main(void)
 {

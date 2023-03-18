430c568f2096becc3d72ee790bd066a71344d815
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-03-18 03:51:57 +0200

    Task12

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 
-int i;
+int n;
 
-for (i = '0'; i <= '9' ; i++)
+for (n = '0'; n <= '9' ; n++)
 {
-putchar(i);
+putchar(n);
 
-if (i != '9')
+if (n != '9')
 {
 putchar(',');
 putchar(' ');

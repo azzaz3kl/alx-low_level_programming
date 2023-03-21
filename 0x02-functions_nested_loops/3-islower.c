5f721235ea85557116766b6fcd69f8c981062431
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-03-22 00:47:18 +0200

    Task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - a function to check character
+ * Return: 1 if true. 0 if false.
+ */
+
+int _islower(int c)
+{
+int i = 'a';
+
+for (i = 'a'; i <= 'z'; i++)
+{
+
+if (c == i)
+{
+return (1);
+}
+}
+return (0);
+}

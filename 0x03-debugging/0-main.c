d0f26e75cff8670bd811d763a13db27abad3e2a5
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-03-22 23:01:34 +0200

    Task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative.
+*
+* positive_or_negative: A function to check number if positive or negative
+*
 * Return: 0
 */
 
@@ -15,12 +18,12 @@ positive_or_negative(i);
 return (0);
 }
 
-void positive_or_negative (int i)
+void positive_or_negative(int i)
 {
-    if (i > 0)
-        printf("%d is positive\n", i);
-    else if (i == 0)
-        printf("%d is zero\n", i);
-    else
-        printf("%d is negative\n", i);
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
+else
+printf("%d is negative\n", i);
 }

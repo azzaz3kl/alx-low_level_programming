88242d7e02902f524a8b85c7c07856a87b0e0f09
Azzaz Akl <azzazibrahimakl@outlook.com>
2023-06-10 05:20:49 +0300

    Getting arguments passed to the program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Getting number of arguments that passed to program
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;                 /* For Ignoring argv */
+	printf("%d\n", argc - 1);
+	return (0);
+}
